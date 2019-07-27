77fd675b10bd0591ea3fd373150bf0d9829feb21
Puttipong1234 <46212403+Puttipong1234@users.noreply.github.com>
2019-07-27 14:30:02 +0700

    "all of this"

---
+++ Sheet.py
@@ -0,0 +1,105 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+class GSdata():
+    scope = ["https://spreadsheets.google.com/feeds",
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("GoodsManager.json", scope)
+
+
+    def __init__(self,product_name = 'Sheet1'):
+
+        self.ManagerName_cell = 'C4'
+        self.place_cell = 'C6'
+        self.product_name_cell = 'C8'
+        self.tax_indentity_cell = 'F5'
+        self.Datetime = datetime.now().strftime("%Y-%m-%d")
+        self.gc = gspread.authorize(GSdata.creds)
+        self.sheet = self.gc.open('GOODS2')
+
+        ### loop to find desire worksheet
+        self.worksheet = self.sheet.worksheets()
+
+        self.product_name = product_name
+
+        if not self.__Check_existing():
+            Feedback = 'ไม่มี Product {} อยู่ขณะนี้ ทำการสร้างตารางใหม่ ?(y/n)'.format(self.product_name)
+            Input = input(Feedback)
+            if Input == 'y':
+                self.create_stock()
+            else :
+                print('Good Luck')
+
+
+        ## check if product exist or not
+    def __Check_existing(self):
+        for num,i in enumerate(self.worksheet):
+            if i == self.product_name:
+                self.worksheet = self.sheet.worksheet(i)
+                return True
+        return False
+
+
+        ## create new worksheet
+    def create_stock(self,Manager_name = 'Pybott',address_name= 'bangkok',tax_iden= '10010'):
+
+        self.sheet.worksheet('Sheet1').duplicate(new_sheet_name = self.product_name)
+        ## set current worksheet
+        self.worksheet = self.sheet.worksheet(self.product_name)
+
+        self.worksheet.update_acell(self.ManagerName_cell,Manager_name)
+
+        self.worksheet.update_acell(self.place_cell,address_name)
+
+        self.worksheet.update_acell(self.product_name_cell,self.product_name)
+
+        self.worksheet.update_acell(self.tax_indentity_cell,tax_iden)
+
+
+        ## add new incoming data
+    def add_product_data(self,number_of_item ,product_number , method = 'รับ',note = None):
+        values_list = self.worksheet.col_values(2)
+        current_row = len(values_list) + 1
+
+        current_row_num = 'B'+str(current_row)
+        self.worksheet.update_acell(current_row_num,product_number)
+
+        current_row_date = 'C'+str(current_row)
+        self.worksheet.update_acell(current_row_date,self.Datetime)
+
+
+        if method == 'รับ':
+            current_row_receive = 'D'+str(current_row)
+            self.worksheet.update_acell(current_row_receive,number_of_item)
+
+        else :
+            current_row_dis = 'E'+str(current_row)
+            self.worksheet.update_acell(current_row_dis,number_of_item)
+        
+        if note is not None:
+            current_row_note = 'G'+str(current_row)
+            self.worksheet.update_acell(current_row_note,note)
+
+        return 'You have updated your products amount'
+    
+
+    # ลบสินค้า 
+    def delete_product_data(self,product_number):
+        cell = self.worksheet.find(str(product_number))
+        self.worksheet.delete_row(cell.row)
+
+        return 'You have delete your products amount'
+
+
+
+if __name__ == '__main__':
+    GSdata('Book')
+    
+    
+
+        
+
